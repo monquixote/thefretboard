65afa99f2581c5b28a8c7a2ff9e443268e0bb73e
monquixote <nl1@NL1MAC.local>
2015-05-14 01:06:04 +0100

    Added locale changes for "Child Categories"

---
+++ htdocs/conf/locale.php
@@ -0,0 +1,5 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Note about what you're translating
+$Definition['Child Categories:'] = "Subcategories:";
+$Definition['Child Categories'] = "Subcategories";
