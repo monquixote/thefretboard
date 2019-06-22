4f63084f4f6ec671a1ab94516fa89f4a7fd2fa24
digitalscream <lee@digitalscream.org.uk>
2019-06-22 18:33:17 +0000

    Style update

--- htdocs/themes/FlatVI/about.php
+++ htdocs/themes/FlatVI/about.php
@@ -2,7 +2,7 @@
 $ThemeInfo['FlatVI'] = array(
    'Name' => 'FlatVI',
    'Description' => "A super sweet port of the popular <a href='http://designmodo.github.io/Flat-UI/'>FlatUI toolkit</a> as a Vanilla theme. This is a fixed width theme.",
-   'Version' => "1.5.98",
+   'Version' => "1.5.99",
    'RequiredApplications' => array('Vanilla' => '2.1a'), //Do not edit me, this has been tested as a requirement
    'Author' => "Chris Ireland & <a href='http://designmodo.com'>DesignModo</a>",
    'AuthorUrl' => 'http://tama63.co.uk'
